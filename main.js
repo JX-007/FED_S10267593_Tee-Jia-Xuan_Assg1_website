4d586977a36be912cab60d52daa3b74bedec9f81
JX-007 <s10267593@connect.np.edu.sg>
2024-11-27 12:55:02 +0800

    Added pictures

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
 
 // Animation on Scroll
 window.addEventListener('scroll', () => {
@@ -35,3 +22,4 @@ buttons.forEach(button => {
         button.style.transform = 'scale(1)';
     });
 });
+
