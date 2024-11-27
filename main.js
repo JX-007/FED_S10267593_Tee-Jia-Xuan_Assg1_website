ad229d1d4824f06afc5559bd73ae1b745eccecd8
JX-007 <s10267593@connect.np.edu.sg>
2024-11-28 01:23:57 +0800

    game page created

--- main.js
+++ main.js
@@ -1,25 +1,2 @@
 
-// Animation on Scroll
-window.addEventListener('scroll', () => {
-    const heroSection = document.querySelector('.hero');
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition > 100) {
-        heroSection.classList.add('animate');
-    } else {
-        heroSection.classList.remove('animate');
-    }
-});
-
-// Button Hover Effect
-const buttons = document.querySelectorAll('.btn');
-buttons.forEach(button => {
-    button.addEventListener('mouseover', () => {
-        button.style.transform = 'scale(1.1)';
-    });
-
-    button.addEventListener('mouseout', () => {
-        button.style.transform = 'scale(1)';
-    });
-});
 
